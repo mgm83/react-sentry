e16958a34ec99fd11cc46cb58440bb039fe88e78
Mauro Morigi <mmorigi@lanacion.com.ar>
2019-02-02 01:46:51 -0300

    add docs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
+import MyError from './MyError';
 
 class App extends Component {
   render() {
@@ -19,6 +21,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Hello />
+          <MyError />
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+const RELEASE = '0.1.0';
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: 'https://aa85197fc11a4bbb9b2fa638e9c55c78@sentry.io/1385366',
+        release: RELEASE,
+    });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
