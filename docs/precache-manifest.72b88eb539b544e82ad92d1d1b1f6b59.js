20b1c6f99746796b79ea6190aba4c5de1cb9bd03
Mauro Morigi <mmorigi@lanacion.com.ar>
2019-02-02 01:59:01 -0300

    update docs

---
+++ docs/precache-manifest.72b88eb539b544e82ad92d1d1b1f6b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sentry/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "11227f1d5d0c05ca3d27",
+    "url": "/react-sentry/static/js/runtime~main.11227f1d.js"
+  },
+  {
+    "revision": "2aa9379af6270d7a1e61",
+    "url": "/react-sentry/static/js/main.2aa9379a.chunk.js"
+  },
+  {
+    "revision": "8030ee4fea9341fd6aef",
+    "url": "/react-sentry/static/js/1.8030ee4f.chunk.js"
+  },
+  {
+    "revision": "2aa9379af6270d7a1e61",
+    "url": "/react-sentry/static/css/main.93b7812c.chunk.css"
+  },
+  {
+    "revision": "54fbaaa22158f1c16e27e3928b582838",
+    "url": "/react-sentry/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8430dcfe096a6a04ad880073ee6715a2.js"
+  "/react-sentry/precache-manifest.72b88eb539b544e82ad92d1d1b1f6b59.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-sentry/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
