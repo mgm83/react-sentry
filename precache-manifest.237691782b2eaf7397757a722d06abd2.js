17eb4f5e8f7b9e914b9d39f4fb2281b794c4c31a
Mauro Morigi <mmorigi@lanacion.com.ar>
2019-02-03 01:28:09 -0300

    Updates

---
+++ precache-manifest.237691782b2eaf7397757a722d06abd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sentry/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "11227f1d5d0c05ca3d27",
+    "url": "/react-sentry/static/js/runtime~main.11227f1d.js"
+  },
+  {
+    "revision": "544fae4bfc2b7e170eee",
+    "url": "/react-sentry/static/js/main.544fae4b.chunk.js"
+  },
+  {
+    "revision": "411d925b570441f4dd2c",
+    "url": "/react-sentry/static/js/1.411d925b.chunk.js"
+  },
+  {
+    "revision": "544fae4bfc2b7e170eee",
+    "url": "/react-sentry/static/css/main.93b7812c.chunk.css"
+  },
+  {
+    "revision": "681d3cf9a242640456beb0f5f6385689",
+    "url": "/react-sentry/index.html"
+  }
+];
